9cd463fe749fe556095c87dc25a2e218a6de971d
LeticiaAndrades15 <leticiadasilvaandrades@gmail.com>
2026-08-17 20:33:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# atividadesPC3
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/LeticiaAndrades15/atividadesPC3)
---
+++ projetos/app.js
@@ -0,0 +1,7 @@
+const conversores = require('./conversores.js');
+
+console.log(conversores.celsiusParaFahrenheit(30));
+console.log(conversores.consumoMedio(500, 40));
+console.log(conversores.horasParaMinutos(2));
+console.log(conversores.idadeEmDias(20));
+console.log(conversores.kmParaMetros(3));
